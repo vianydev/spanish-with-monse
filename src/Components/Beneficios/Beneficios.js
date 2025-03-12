bcd22c59f988640c89684af8c1c67727acef20ff
vianey <vianymarcelo@gmail.com>
2025-03-11 19:46:30 -0600

    fix icons

--- src/Components/Beneficios/Beneficios.js
+++ src/Components/Beneficios/Beneficios.js
@@ -6,6 +6,7 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faComments } from '@fortawesome/free-solid-svg-icons';
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import { faThumbTack } from '@fortawesome/free-solid-svg-icons';
+// import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { Trans } from 'react-i18next';
 
 const Beneficios = ({ t }) => {
@@ -25,60 +26,60 @@ const Beneficios = ({ t }) => {
           <div className="beneficios-box">
             <div className="beneficios-img">
               <FontAwesomeIcon className="icon-beneficios" icon={faClock} />
-            </div>
-            <div className="main.beneficios-box-text">
-              <h4>{t('main.benefits.Rhythm')}</h4>
-              <p>{t('main.benefits.RhythmText')}</p>
+              <div className="main.beneficios-box-text">
+                <h4>{t('main.benefits.Rhythm')}</h4>
+                <p>{t('main.benefits.RhythmText')}</p>
+              </div>
             </div>
           </div>
           {/* Conversaciones reales */}
           <div className="beneficios-box">
             <div className="beneficios-img">
               <FontAwesomeIcon className="icon-beneficios" icon={faComments} />
-            </div>
-            <div className="beneficios-box-text">
-              <h4>{t('main.benefits.Conversations')}</h4>
-              <p>{t('main.benefits.ConversationsText')}</p>
+              <div className="beneficios-box-text">
+                <h4>{t('main.benefits.Conversations')}</h4>
+                <p>{t('main.benefits.ConversationsText')}</p>
+              </div>
             </div>
           </div>
           {/* Desde cualquier lugar */}
           <div className="beneficios-box">
             <div className="beneficios-img">
               <FontAwesomeIcon className="icon-beneficios" icon={faLocationDot} />
-            </div>
-            <div className="beneficios-box-text">
-              <h4>{t('main.benefits.Place')}</h4>
-              <p>{t('main.benefits.PlaceText')}</p>
+              <div className="beneficios-box-text">
+                <h4>{t('main.benefits.Place')}</h4>
+                <p>{t('main.benefits.PlaceText')}</p>
+              </div>
             </div>
           </div>
           {/* Just in Desktop */}
           <div className="beneficios-box beneficios-desktop">
             <div className="beneficios-img">
               <FontAwesomeIcon className="icon-beneficios" icon={faListCheck} />
-            </div>
-            <div className="beneficios-box-text">
-              <h4>{t('main.benefits.Plan')}</h4>
-              <p>{t('main.benefits.PlanText')}</p>
+              <div className="beneficios-box-text">
+                <h4>{t('main.benefits.Plan')}</h4>
+                <p>{t('main.benefits.PlanText')}</p>
+              </div>
             </div>
           </div>
           {/* temas de tu interes */}
           <div className="beneficios-box beneficios-desktop">
             <div className="beneficios-img">
               <FontAwesomeIcon className="icon-beneficios" icon={faThumbTack} />
-            </div>
-            <div className="beneficios-box-text">
-              <h4>{t('main.benefits.Topics')}</h4>
-              <p>{t('main.benefits.TopicsText')}</p>
+              <div className="beneficios-box-text">
+                <h4>{t('main.benefits.Topics')}</h4>
+                <p>{t('main.benefits.TopicsText')}</p>
+              </div>
             </div>
           </div>
           {/* Resuelve tus dudas */}
           <div className="beneficios-box beneficios-desktop">
             <div className="beneficios-img">
               <FontAwesomeIcon className="icon-beneficios" icon={faLightbulb} />
-            </div>
-            <div className="Resuelve-tus-dudas">
-              <h4>{t('main.benefits.Questions')}</h4>
-              <p>{t('main.benefits.QuestionsText')}</p>
+              <div className="Resuelve-tus-dudas">
+                <h4>{t('main.benefits.Questions')}</h4>
+                <p>{t('main.benefits.QuestionsText')}</p>
+              </div>
             </div>
           </div>
         </div>
